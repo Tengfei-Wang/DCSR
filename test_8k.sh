e736478d24c94160a3b1cffb7e664c34ba04b62f
Tengfei Wang <tfwang@cse.ust.hk>
2021-09-04 19:48:23 +0800

    Update test_8k.sh

--- test_8k.sh
+++ test_8k.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=9 python3 main.py 	--pre_train  './experiments/pretrained/model_basic.pt' \
+CUDA_VISIBLE_DEVICES=9 python3 main.py 	--pre_train  './experiments/pretrain/model_basic.pt' \
 					--save test_8k \
 					--dir_test_LR './data/CameraFusion/test/HR/' \
 					--dir_test_ref './data/CameraFusion/test/Ref/x1/' \
