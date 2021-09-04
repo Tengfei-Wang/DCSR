321f51f1664c816a98582a98d93376b6affbf463
Tengfei Wang <tfwang@cse.ust.hk>
2021-09-04 19:45:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=9  python3 main.py --pre_train  './experiments/basic/model/model_latest.pt'  \
+CUDA_VISIBLE_DEVICES=9  python3 main.py --pre_train  './experiments/pretrained/model_basic.pt'  \
                         --save test_4k \
                         --test_only \
                         --n_feats 64
+                        
+# --pre_train './experiments/basic/model/model_latest.pt'
