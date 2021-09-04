095a42a8902cc6476a0d96cb10a1534f88d71b87
Tengfei Wang <tfwang@cse.ust.hk>
2021-09-04 19:47:43 +0800

    Update test_8k_SRA.sh

--- test_8k_SRA.sh
+++ test_8k_SRA.sh
@@ -1,7 +1,9 @@
-CUDA_VISIBLE_DEVICES=9 python3 main.py --pre_train './experiments/SRA/model/model_latest.pt'  \
+CUDA_VISIBLE_DEVICES=9 python3 main.py --pre_train './experiments/pretrain/model_SRA.pt'  \
 										--save test_8k_SRA \
 										--dir_test_LR './data/CameraFusion/test/HR/' \
 										--dir_test_ref './data/CameraFusion/test/Ref/x1/' \
 										--test_only  \
 										--flag_8k \
-                    --n_feats 64
+                    								--n_feats 64
+
+#--pre_train './experiments/SRA/model/model_latest.pt'
