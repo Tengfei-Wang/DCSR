1e1b94b4e347412956ea6ecfae4abf054b0d5ff8
Tengfei Wang <tfwang@cse.ust.hk>
2021-09-05 22:32:46 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=9 python3 main.py  \
                               --save basic \
                               --save_model \
-          										--dir_test_LR './data/CameraFusion/val/LR/x2' \
-          										--dir_test_ref './data/CameraFusion/val/Ref/x2/' \                              
+                              --dir_test_LR './data/CameraFusion/val/LR/x2' \
+                              --dir_test_ref './data/CameraFusion/val/Ref/x2/' \                              
                               --loss 1*L1+0.05*contextual_ref+0.01*contextual_hr \
                               --patch_size 128 \
                               --batch_size 4 \
